f8bde0288918dab85a9d85ac6d1ed5b82f07fe1a
Daewon Kim <daewonkim@beringlab.com>
2022-03-04 15:24:42 +0900

    chore: update progress
    include chinese test

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # spacy practice
 
 tested on m1 mac
+
+## NOTE
+
+failed install japanese using `spacy download ja_core_news_sm`
--- src/main.py
+++ src/main.py
@@ -1,4 +1,56 @@
 import spacy
+# from spacy.lang.zh.examples import sentences
 
-spacy.prefer_gpu()
-nlp = spacy.load("en_core_web_sm")
+# spacy.prefer_gpu()
+# nlp = spacy.load("en_core_web_sm")
+
+def sentencizer(input_sentence: str):
+    print(f"original sentence before sentencizer :{input_sentence}")
+
+    doc = nlp(input_sentence)
+
+    for idx, sent in enumerate(doc.sents):
+        print(f'{idx} {sent}')
+
+
+if __name__ == '__main__':
+    global nlp
+
+    nlp = spacy.load('zh_core_web_sm')
+    config = {"punct_chars": None}
+    nlp.add_pipe("sentencizer", config=config)
+    
+    """
+    use translation result from google-translator.
+    include dot (end of sentence)
+
+    kor: "안녕하세요 반갑습니다. 저는 아기 요다입니다."
+    eng: "Hello nice to meet you. I am baby yoda."
+    """
+
+    print("🏁 inlcude dot case.")
+    sentencizer('''
+        你好，很高兴认识你。我是宝贝尤达。
+    ''')
+
+    """
+    exclude dot
+
+    eng: Hello nice to meet you I am baby yoda
+    """
+
+    print("🏁 exlcude dot case.")
+    sentencizer('''
+        你好很高兴认识你我是宝贝尤达
+    ''')
+
+    """
+    exclude dot
+
+    kor: 안녕하세요 반갑습니다 저는 아기 요다입니다
+    """
+
+    print("🏁 exlcude dot case.")
+    sentencizer('''
+        你好很高兴认识你我是尤达宝贝
+    ''')
